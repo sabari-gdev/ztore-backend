28ab044f514106e0fb9f1e8b69564dd73c2b77d7
SABARINATH P S <sabarinath.p@flycatchtech.com>
2024-02-14 13:09:28 +0530

    Server initialised and now it is running

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,31 @@
 import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello NOD Readers!");
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  dotenv.config();
+}
+const PORT = process.env.PORT || 7000;
+
+process.on("uncaughtException", (err) => {
+  console.log(
+    "Shutting down the server for the uncaught exception: ",
+    err.message
+  );
 });
 
-app.listen(port, () => {
-  return console.log(
-    `Express server is listening at http://localhost:${port} 🚀`
+const server = app.listen(PORT, () => {
+  console.log("Server is running at the port: ", PORT);
+});
+
+process.on("unhandledRejection", (err: Error) => {
+  console.log(
+    "Server is shutting down due to unhandled rejection: ",
+    err.message
   );
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
